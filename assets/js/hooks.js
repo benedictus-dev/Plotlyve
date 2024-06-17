87702e53a54a07fe5aff5cfe7758687e6f379c6a
Benedictus Yevu <benedictusyevu@Benedictuss-MacBook-Pro.local>
2024-06-17 02:55:48 +0000

    Init js setup

---
+++ assets/js/hooks.js
@@ -0,0 +1,12 @@
+import HistoChart from "./histo-chart";
+
+let Hooks = {
+  HistoChart: HistoChart = {
+    mounted() {},
+    updated() {},
+    destroyed() {},
+  },
+};
+
+
+export default Hooks;
